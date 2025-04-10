6c2f028584770bbfad2323f4d4ff57969b56ab8b
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-04-10 15:45:55 -0300

    Removendo o arquivo db.py que continha dados de exemplo, para evitar armazenamento em memória desnecessário

--- backend/src/database/db.py
+++
@@ -1,7 +0,0 @@
-dados = [
-        {'id':1, 'nome': 'Patrick', 'cargo': 'ceo', 'cracha': 123456},
-        {'id':2, 'nome': 'Lucas', 'cargo': 'dev', 'cracha': 654321},
-        {'id':3, 'nome': 'João', 'cargo': 'dev', 'cracha': 89456},
-        {'id':4, 'nome': 'Maria', 'cargo': 'dev', 'cracha': 123789},
-        {'id':5, 'nome': 'Ana', 'cargo': 'dev', 'cracha': 456123}
-]
