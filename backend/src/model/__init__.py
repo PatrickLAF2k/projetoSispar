dd02168f4bdac7d6571ed382e2cf9a8feae2299f
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-04-10 15:46:04 -0300

    Adicionando comentários explicativos ao arquivo __init__.py para melhorar a compreensão do código

--- backend/src/model/__init__.py
+++ backend/src/model/__init__.py
@@ -1,4 +1,7 @@
-#Arquivo responsavel por instanciar o banco de dados
+# Importa a classe SQLAlchemy do pacote flask_sqlalchemy.
+# Essa extensão integra o SQLAlchemy com o Flask, facilitando o uso de banco de dados relacionais.
 from flask_sqlalchemy import SQLAlchemy
 
+# Cria uma instância global do SQLAlchemy.
+# Essa instância será usada para configurar e manipular o banco de dados ao longo do seu projeto Flask.
 db = SQLAlchemy()
